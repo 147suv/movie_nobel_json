330a17e4bc2256a448bd3cfa077b347576dc99af
victor unuese <victor2020richard@gmail.com>
2019-01-28 19:44:44 +0200

    first commit

---
+++ movies API/main.js
@@ -0,0 +1,6 @@
+$input.addEventListener("keyup",function(){
+    console.log("hello")
+    let $filter_val = $input.value.toUpperCase()
+    console.log($filter_val)
+    let $result = document.getElementById("result")
+})
